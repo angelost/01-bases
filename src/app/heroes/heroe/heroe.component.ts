6a11651e1ec8638247ee4df5f8a38db2af75aa60
Angelo <angelo.gonzalez@live.com>
2022-07-01 14:58:09 -0400

    43. Cambios en el template del componente

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -5,6 +5,16 @@ import { Component } from "@angular/core";
     selector: 'app-heroe',
     templateUrl: 'heroe.component.html'
 })
-export class HeroeComponent{
+export class HeroeComponent {
+    nombre: string = 'Ironman';
+    edad  : number = 45
+
+    get nombreCapitalizado(): string {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${ this.nombre } - ${ this.edad }`;
+    }
 
 }
