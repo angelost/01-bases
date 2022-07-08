5c2d8b9a835d6628d5676cdc12be5a50f9f336a3
Angelo <angelo.gonzalez@live.com>
2022-07-07 21:32:12 -0400

    59. ngModel

--- src/app/dbz/main-paige/main-paige.component.ts
+++ src/app/dbz/main-paige/main-paige.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-paige',
   templateUrl: './main-paige.component.html'
 })
 export class MainPaigeComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
   agregar() {
-    console.log('Esta es una prueba');
+    console.log(this.nuevo);
   }
 
 }
